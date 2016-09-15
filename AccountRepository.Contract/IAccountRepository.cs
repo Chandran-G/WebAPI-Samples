f2227e0acd571563e5e2bd3334092077de8fbe16
vijay <vijay@VIJAY-HP>
2016-09-15 16:38:30 -0400

    Add project files.

---
+++ AccountRepository.Contract/IAccountRepository.cs
@@ -0,0 +1,46 @@
+﻿using Account.DomainModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Account.Repository.Contract
+{
+    public interface IAccountRepository
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        IEnumerable<UserAccount> GetAll();
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        UserAccount GetUserAccount(int id);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns></returns>
+        UserAccount Add(UserAccount item);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="id"></param>
+        void Remove(int id);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns></returns>
+        bool Update(UserAccount item);
+    }
+}
+
